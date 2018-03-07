47080014fbce380495b82343b2c3da6a0a3a53f6
Fernando Herrera <faherrera2@gmail.com>
2018-03-06 22:34:39 -0500

    Limpieza de datos

--- textMiningR.R
+++ textMiningR.R
@@ -12,11 +12,15 @@ preprocesamientoDatos = function(corpusPlanes){
   removeURL <- function(x) gsub("http[[:alnum:][:punct:]]*", "", x) 
   corpusPlanes = tm_map(corpusPlanes, content_transformer(removeURL))
   
+  # Remover caracteres especiales
+  removeCarateresEsp = function(x) gsub("[[:cntrl:]]", " ", x)
+  corpusPlanes = tm_map(corpusPlanes, content_transformer(removeCarateresEsp))
+  
   #espacio a los signos de puntuacion palabra(palabra)
   espacio = content_transformer(function(x, pattern) gsub(pattern, " ", x))
   corpusPlanes = tm_map(corpusPlanes, espacio, "[[:punct:]]+")
   
-  #Removemos los signos de puntuaci??n
+  #Removemos los signos de puntuación
   corpusPlanes = tm_map(corpusPlanes, removePunctuation)
   #Removemos los numeros
   corpusPlanes = tm_map(corpusPlanes, removeNumbers)
@@ -46,26 +50,14 @@ corpusP1T1 = VCorpus(DirSource("//home//faherrera2//Dropbox//Tesis//corpusPlanes
 
 
 corpusPlanes = preprocesamientoDatos(corpusP1T1)
-writeCorpus(corpusPlanes, path = "//home//faherrera2//Documents//planesProcesados")
+#writeCorpus(corpusPlanes, path = "//home//faherrera2//Documents//planesProcesados")
 
 corpusMatriz = corpusPlanes
 tdmUni = TermDocumentMatrix(corpusMatriz)
+tdmUni_matriz = as.matrix(tdmUni)
 
 
-
-BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
 
 tdm <- TermDocumentMatrix(corpusMatriz, control = list(tokenize = BigramTokenizer))
 
-inspect(tdm)
-tdm$dimnames$Terms[20:60]
-matriz = as.matrix(tdm)
-
-hunspell(corpusP1T1[[1]], format = "text")
-
-stopWord = hunspell_parse(readLines(list.files()[1], encoding = "UTF-8"))
-for(document in list.files()){
-  
-}
-
-palabras = tm_map(c("árboles", "caminando", "busqueda"), stemDocument)
+BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
